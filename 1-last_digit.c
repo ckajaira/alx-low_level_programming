f48a24ee20a15ae74007f1c38bb9b207ed1320f2
Cindy <cindykajaira@gmail.com>
2023-02-17 14:49:44 -0800

    Task 1

---
+++ 1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* 
+ * main - Prints the last digit randomly generated number.
+ * and whatever it is greater than 5, less than 6, or 0.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+         int n;
+
+         srand(time(0));
+         n = rand() - RAND_MAX / 2;
+         if ((n % 10) > 5)
+         {
+                 printf("Last digit of %d is %d and is greater than 5\n",
+                         n, n%10);
+         }
+         else if ((n % 10) < 6 && (n % 10) !- 0)
+         {
+                 printf("Last digit of %d is %d and is less than 66and not 0\n",
+                         n, n%10);
+         }
+         else
+         {
+                 printf("Last digit of %d is %d and is 0\n",
+                         n, n%10);
+         }
+         return (0);
+} 
